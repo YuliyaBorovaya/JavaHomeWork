63a59c95ebb5aeeef0596ac09876b9a2c0094628
YuliyaBorovaya <109955677+YuliyaBorovaya@users.noreply.github.com>
2022-12-08 00:19:57 +0300

    Update Task3.java

--- HomeWork1/Task3.java
+++ HomeWork1/Task3.java
@@ -33,33 +33,4 @@ public class Task3 {
 }
 
 
-// // import java.util.Scanner;
-
-// // Реализовать простой калькулятор
-// public class App3 {
-//     public static void main(String[] args) {
-//         System.out.print("Введите первое число: ");
-//         Scanner sc=new Scanner(System.in);
-//         int numb_1=sc.nextInt();   
-//         System.out.print("Введите второе число: ");
-//         int numb_2=sc.nextInt();
-//         System.out.print("Введите математическое действие: ");
-//         sc.nextLine();
-//         String action=sc.nextLine();                     
-//         sc.close();
-//         if (action.equals("+")) {
-//             System.out.print(numb_1+numb_2);
-//         }
-//         else if(action.equals("-")){
-//             System.out.print(numb_1-numb_2);
-//         }
-//         else if(action.equals("*")){
-//             System.out.print(numb_1*numb_2);
-//         }
-//         else if(action.equals("/") && numb_2 != 0){
-//             System.out.print(numb_1/numb_2);
-//         }else{
-//             System.out.print("Не знаю, что от меня хотите");
-//         }
-//     }
-// }
+    
